fb37fae6de9a813f13174cc0b7a5cb8d73b1e922
wangicheng <r2005.wang@gmail.com>
2017-07-12 11:52:51 +0800

    Pi_in_Python

--- math1.py
+++ math1.py
@@ -7,7 +7,7 @@ y=int(0)
 """sqrt(n)-number"""
 def sqr_n(n):
     global y,g
-    ans=abs(math.sqrt(n)-math.sqrt(n)//1+3)
+    ans=abs((math.sqrt(n)-math.sqrt(n)//1)+3)
     if abs(ans-math.pi)<g :
         g=abs(ans-math.pi)
         print("√"+str(n)+"-"+str(int(math.sqrt(n)//1-3))+"="+str(ans))
